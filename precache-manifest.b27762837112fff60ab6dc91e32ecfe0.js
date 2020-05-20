b330cb92a1d527ba0984520389d1f22f646de1bd
Kpoke <danielolojakpoke@gmail.com>
2020-05-20 01:14:57 +0100

    Updates

---
+++ precache-manifest.b27762837112fff60ab6dc91e32ecfe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f50f8f3cbd0a5120c7cf31b79a06cdd",
+    "url": "/debtors/index.html"
+  },
+  {
+    "revision": "b9d3c3b2cac1b89dac8b",
+    "url": "/debtors/static/css/main.fce0787e.chunk.css"
+  },
+  {
+    "revision": "2f547c05416ba283159b",
+    "url": "/debtors/static/js/2.543cf5de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/debtors/static/js/2.543cf5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d3c3b2cac1b89dac8b",
+    "url": "/debtors/static/js/main.02a66f71.chunk.js"
+  },
+  {
+    "revision": "ef32bd4f81d44b443d3a",
+    "url": "/debtors/static/js/runtime-main.51d34261.js"
+  }
+]);
